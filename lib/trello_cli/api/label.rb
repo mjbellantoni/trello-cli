ba38747c5f555b844182ac314c30fb6a5cb1a014
Matthew Bellantoni <mjbellantoni@gmail.com>
2026-08-16 08:43:35 -0400

    Add Api::Label with uniqueness and delete guards

---
+++ lib/trello_cli/api/label.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+class TrelloCli::Api::Label
+  def self.all(client, config)
+    client.get("/boards/#{config.board_id}/labels")
+  end
+
+  def self.find_by_name(client, config, name)
+    utf8 = name.dup.force_encoding("UTF-8").downcase
+    label = all(client, config).find { |l| l["name"].to_s.downcase == utf8 }
+    raise TrelloCli::NotFoundError, "Label not found: #{name}" unless label
+
+    label
+  end
+
+  def self.exists?(client, config, name)
+    utf8 = name.dup.force_encoding("UTF-8").downcase
+    all(client, config).any? { |l| l["name"].to_s.downcase == utf8 }
+  end
+
+  def self.create(client, config, name:, color:)
+    if exists?(client, config, name)
+      raise TrelloCli::Error, "Label already exists: #{name}. Names must be unique to stay addressable."
+    end
+
+    client.post("/labels", { name: name, color: color, idBoard: config.board_id })
+  end
+
+  def self.rename(client, config, name, new_name)
+    label = find_by_name(client, config, name)
+    if exists?(client, config, new_name)
+      raise TrelloCli::Error, "Label already exists: #{new_name}. Names must be unique to stay addressable."
+    end
+
+    client.put("/labels/#{label['id']}", { name: new_name })
+  end
+
+  def self.cards_using(client, config, label_id)
+    cards = client.get("/boards/#{config.board_id}/cards", { fields: "labels" })
+    cards.count { |card| (card["labels"] || []).any? { |l| l["id"] == label_id } }
+  end
+
+  def self.delete(client, config, name)
+    label = find_by_name(client, config, name)
+    count = cards_using(client, config, label["id"])
+
+    if count.positive? && ENV["TRELLO_ALLOW_LABEL_DELETE_IN_USE"] != "true"
+      raise TrelloCli::Error,
+            "\"#{name}\" is on #{count} card#{'s' if count != 1}. Deleting removes it from all of them.\n" \
+            "Set TRELLO_ALLOW_LABEL_DELETE_IN_USE=true to permit this."
+    end
+
+    client.delete("/labels/#{label['id']}")
+  end
+end
---
+++ spec/trello_cli/api/label_spec.rb
@@ -0,0 +1,110 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe TrelloCli::Api::Label do
+  let(:config) do
+    instance_double(TrelloCli::Api::Config, api_key: "test_key", token: "test_token", board_id: "test_board")
+  end
+  let(:client) { TrelloCli::Api::Client.new(config) }
+  let(:auth) { { key: "test_key", token: "test_token" } }
+
+  def stub_labels(labels)
+    stub_request(:get, "https://api.trello.com/1/boards/test_board/labels")
+      .with(query: auth)
+      .to_return(status: 200, body: labels.to_json, headers: { "Content-Type" => "application/json" })
+  end
+
+  before do
+    stub_labels([{ "id" => "l1", "name" => "bug", "color" => "red" },
+                 { "id" => "l2", "name" => "", "color" => "sky" }])
+  end
+
+  describe ".find_by_name" do
+    it "finds a label case-insensitively" do
+      expect(described_class.find_by_name(client, config, "BUG")["id"]).to eq("l1")
+    end
+
+    it "raises when the label does not exist" do
+      expect {
+        described_class.find_by_name(client, config, "nope")
+      }.to raise_error(TrelloCli::NotFoundError, /nope/)
+    end
+  end
+
+  describe ".create" do
+    it "posts a new label" do
+      stub_request(:post, "https://api.trello.com/1/labels")
+        .with(query: auth).to_return(status: 200, body: { "id" => "l9" }.to_json,
+                                     headers: { "Content-Type" => "application/json" })
+
+      described_class.create(client, config, name: "chore", color: "blue")
+
+      expect(
+        a_request(:post, "https://api.trello.com/1/labels")
+          .with(query: auth, body: { name: "chore", color: "blue", idBoard: "test_board" })
+      ).to have_been_made.once
+    end
+
+    it "refuses a duplicate name" do
+      expect {
+        described_class.create(client, config, name: "Bug", color: "blue")
+      }.to raise_error(TrelloCli::Error, /already exists/i)
+    end
+  end
+
+  describe ".rename" do
+    it "refuses to rename onto an existing name" do
+      stub_labels([{ "id" => "l1", "name" => "bug", "color" => "red" },
+                   { "id" => "l2", "name" => "chore", "color" => "blue" }])
+      expect {
+        described_class.rename(client, config, "bug", "chore")
+      }.to raise_error(TrelloCli::Error, /already exists/i)
+    end
+  end
+
+  describe ".delete" do
+    def stub_board_cards(cards)
+      stub_request(:get, "https://api.trello.com/1/boards/test_board/cards")
+        .with(query: hash_including(auth))
+        .to_return(status: 200, body: cards.to_json, headers: { "Content-Type" => "application/json" })
+    end
+
+    it "deletes a label that is on no cards" do
+      stub_board_cards([{ "id" => "c1", "labels" => [] }])
+      stub_request(:delete, "https://api.trello.com/1/labels/l1")
+        .with(query: auth).to_return(status: 200, body: "{}", headers: { "Content-Type" => "application/json" })
+
+      described_class.delete(client, config, "bug")
+
+      expect(a_request(:delete, "https://api.trello.com/1/labels/l1").with(query: auth)).to have_been_made.once
+    end
+
+    it "refuses to delete a label that is in use" do
+      stub_board_cards([{ "id" => "c1", "labels" => [{ "id" => "l1" }] }])
+      expect {
+        described_class.delete(client, config, "bug")
+      }.to raise_error(TrelloCli::Error, /1 card/)
+    end
+
+    it "names the environment override rather than a flag" do
+      stub_board_cards([{ "id" => "c1", "labels" => [{ "id" => "l1" }] }])
+      expect {
+        described_class.delete(client, config, "bug")
+      }.to raise_error(TrelloCli::Error, /TRELLO_ALLOW_LABEL_DELETE_IN_USE/)
+    end
+
+    it "deletes an in-use label when the override is set" do
+      stub_board_cards([{ "id" => "c1", "labels" => [{ "id" => "l1" }] }])
+      stub_request(:delete, "https://api.trello.com/1/labels/l1")
+        .with(query: auth).to_return(status: 200, body: "{}", headers: { "Content-Type" => "application/json" })
+
+      ENV["TRELLO_ALLOW_LABEL_DELETE_IN_USE"] = "true"
+      described_class.delete(client, config, "bug")
+
+      expect(a_request(:delete, "https://api.trello.com/1/labels/l1").with(query: auth)).to have_been_made.once
+    ensure
+      ENV.delete("TRELLO_ALLOW_LABEL_DELETE_IN_USE")
+    end
+  end
+end
